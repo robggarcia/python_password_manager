77949cc7f9ca9207f60a8500bad7c7a37c635482
robggarcia <robert.g.garcia82@gmail.com>
2023-01-17 20:15:56 -0500

    password manager

--- main.py
+++ main.py
@@ -2,8 +2,33 @@
 
 from tkinter import *
 from tkinter import messagebox
+from random import randint, shuffle, choice
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def create_password():
+    pass_entry.delete(0, END)
+    nr_letters = randint(8, 10)
+    nr_symbols = randint(2, 4)
+    nr_numbers = randint(2, 4)
+
+    pass_letters = [choice(letters) for _ in range(nr_letters)]
+    pass_symbols = [choice(symbols) for _ in range(nr_symbols)]
+    pass_numbers = [choice(numbers) for _ in range(nr_numbers)]
+
+    pass_list = pass_letters + pass_symbols + pass_numbers
+
+    shuffle(pass_list)
+    new_password = "".join(pass_list)
+    pass_entry.insert(0, new_password)
+    # copy the new password to the users clipboard
+    pyperclip.copy(new_password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -13,15 +38,19 @@ def add_password():
     user = user_entry.get()
     password = pass_entry.get()
 
-    # create a message box for the user
-    is_ok = messagebox.askokcancel(
-        title=website, message=f"These are the details entered:\n{user}\n{password}\nIs it OK to save?")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Oops", message="Please don't leave any fields empty.")
+    else:
+        # create a message box for the user
+        is_ok = messagebox.askokcancel(
+            title=website, message=f"These are the details entered:\n{user}\n{password}\nIs it OK to save?")
 
-    if is_ok:
-        with open("data.txt", "a") as my_file:
-            my_file.write(f"{website} | {user} | {password}\n")
-        website = web_entry.delete(0, END)
-        password = pass_entry.delete(0, END)
+        if is_ok:
+            with open("data.txt", "a") as my_file:
+                my_file.write(f"{website} | {user} | {password}\n")
+            website = web_entry.delete(0, END)
+            password = pass_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -50,7 +79,7 @@ user_entry.grid(row=2, column=1, columnspan=2)
 pass_label = Label(text="Password:").grid(row=3, column=0)
 pass_entry = Entry(width=18)
 pass_entry.grid(row=3, column=1)
-pass_button = Button(text="Generate Password").grid(
+pass_button = Button(text="Generate Password", command=create_password).grid(
     row=3, column=2)
 # add
 add_button = Button(text="Add", width=33, command=add_password).grid(
